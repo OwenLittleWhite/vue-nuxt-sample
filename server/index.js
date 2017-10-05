eb608beae0c5a0b87c82e0507651dbf93e30ebd2
Owen <253065903@qq.com>
2017-10-05 11:51:12 +0800

    add body-parser

--- server/index.js
+++ server/index.js
@@ -2,13 +2,16 @@ import express from 'express'
 import { Nuxt, Builder } from 'nuxt'
 
 import api from './api'
+import bodyParser from 'body-parser' 
 
 const app = express()
 const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 3000
 
-app.set('port', port)
 
+app.set('port', port)
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 // Import API Routes
 app.use('/api', api)
 
